7bd194dca293099a40b460c2631679b10f599998
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-01 09:21:37 +0100

    created the fuction in showing in active in grid

--- js/script.js
+++ js/script.js
@@ -3,8 +3,26 @@ const buttonPlay = document.querySelector(".btn");
 
 buttonPlay.addEventListener('click', function () {
 
+
+const divBoxElement = document.querySelector(".box");
+divBoxElement.classList.add('custom_border');
+
+for (let i = 0; i < 100; i++) {
+    const newSquare = getNewSquare();
+    divBoxElement.appendChild(newSquare);
+    newSquare.append(i + 1);
+}
+
 })
 
+function getNewSquare() {
+    const newSquare = document.createElement("div");
+    newSquare.classList.add('square');
+    newSquare.addEventListener("click", function () {
+    newSquare.classList.toggle('clicked');
+    })
+    return newSquare;
+}  
 
 
 
